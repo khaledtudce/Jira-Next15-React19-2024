efad67b388112039326544cd3ac8868973f8ffaa
khaled Reza <khaledreza@gmail.com>
2024-11-25 19:51:28 +0100

    Re-designed user menu using clerk

--- components/header.jsx
+++ components/header.jsx
@@ -1,15 +1,41 @@
-import { SignedIn, SignedOut, SignInButton, UserButton } from "@clerk/nextjs";
+import { SignedIn, SignedOut, SignInButton } from "@clerk/nextjs";
+import Image from "next/image";
+import Link from "next/link";
+import { Button } from "./ui/button";
+import { PenBox } from "lucide-react";
+import UserMenu from "./user-menu";
 
 const Header = () => {
   return (
-    <div>
-      <SignedOut>
-        <SignInButton />
-      </SignedOut>
-      <SignedIn>
-        <UserButton />
-      </SignedIn>
-    </div>
+    <header className="container mx-auto">
+      <nav className="py-6 px-4 flex justify-between items-center">
+        <Link href="/">
+          <Image
+            className="h-10 w-auto object-contain"
+            src={"/logo2.png"}
+            alt="Scrum Logo"
+            width={200}
+            height={56}
+          />
+        </Link>
+        <div className="flex items-center gap-4">
+          <Link href="/project/create">
+            <Button variant="destructive">
+              <PenBox size={18} />
+              <span>Create Project</span>
+            </Button>
+          </Link>
+          <SignedOut>
+            <SignInButton forceRedirectUrl="/onboarding">
+              <Button variant="outline">Login</Button>
+            </SignInButton>
+          </SignedOut>
+          <SignedIn>
+            <UserMenu />
+          </SignedIn>
+        </div>
+      </nav>
+    </header>
   );
 };
 
