d61475c101505cc473e07eda784c3356fc64a1dc
khaled Reza <khaledreza@gmail.com>
2024-11-27 20:58:39 +0100

    create routing file structures and not found file

---
+++ app/not-found.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const NotFound = () => {
+  return (
+    <div className="text-4xl font-extrabold w-screen pt-96 grid place-items-center">
+      My 404: not-found
+    </div>
+  );
+};
+
+export default NotFound;
